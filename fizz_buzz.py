f0dc0d87b16ad107e99dc6e31f70fef31a1dd7ce
Lawrence Sithole <lawrence.sithole@defra.gov.uk>
2026-02-05 13:41:00 +0000

    completed pytest fizz-buzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,2 +1,8 @@
 def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz 🎉"
+    if number % 3 == 0:
+        return "Fizz 🍾"
+    if number % 5 == 0:
+        return "Buzz 🐝"
     return number
--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -2,3 +2,14 @@ from fizz_buzz import fizz_buzz
 
 def test_just_number():
     assert fizz_buzz(1) == 1
+
+def test_fizz():
+    assert fizz_buzz(3) == "Fizz 🍾"
+    #assert fizz_buzz(6) == "Fizz 🍾"
+
+def test_buzz():
+    assert fizz_buzz(5) == "Buzz 🐝"
+
+def test_fizz_buzz():
+    assert fizz_buzz(15) == "FizzBuzz 🎉"
+    #assert fizz_buzz(3) == "Fizz"   
